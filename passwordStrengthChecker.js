042319cc847168384ac2b2d64f9b72f0873c6009
Janek <j.gulkowski@wp.pl>
2016-04-21 02:11:53 +0200

    Plugin done

--- passwordStrengthChecker.js
+++ passwordStrengthChecker.js
@@ -5,10 +5,48 @@
 (function($) {  //konstrukcja (function($) { ... } )(jQuery) zapobiega kolidowania z innymi bibliotekami również
                 // wykorzystującymi znak $
 
-    function passwordContainsCharacters(password, lowerBound, upperBound) {
-        for (i = 0; i < password.length; i++) {
-            if (password.charAt(i) >= lowerBound && password.charAt(i) <= upperBound) {
-                console.log("password contains character beetween " + lowerBound + " and " + upperBound);
+    function createPasswordStrengthMeter(passwordField, passwordStrengthMeterId) {
+        var passwordStrengthMeter = document.createElement('meter');  //tworzymy html'owy obiekt <meter>
+        passwordStrengthMeter.setAttribute('id', passwordStrengthMeterId);  //ustawiamy atrybut id = password-strength-meter
+        var parent = passwordField.parent();    //wyciągamy rodzica passwordFielda dla któego odpalilismy nasz plugin
+        parent.append(passwordStrengthMeter);   //zeby dodac do niego nowo stworzony obiekt <meter>
+    }
+
+    function modifyPasswordStrengthMeter(passwordValue, entropyOfTheStrongPassword, passwordStrengthMeterId) {
+        var entropyOfThePassword = calculateEntropyOfThePassword(passwordValue);
+        var strengthOfThePassword = caluculateStrengthOfThePassword(entropyOfThePassword, entropyOfTheStrongPassword);
+        displayTheBar(strengthOfThePassword, passwordStrengthMeterId);
+    }
+
+    function calculateEntropyOfThePassword(passwordValue) {
+        var numberOfPossibleCharacters = 0;
+        if (passwordContainsCharacters(passwordValue, 'a', 'z')) {
+            numberOfPossibleCharacters += 26;
+        }
+        if (passwordContainsCharacters(passwordValue, 'A', 'Z')) {
+            numberOfPossibleCharacters += 26;
+        }
+        if (passwordContainsCharacters(passwordValue, '0', '9')) {
+            numberOfPossibleCharacters += 10;
+        }
+        if (areSpecialCharactersInPassword(passwordValue)) {
+            numberOfPossibleCharacters += 33;
+        }
+        //console.log('number of possible chars: ' + numberOfPossibleCharacters);   //sprawdzacz
+        var entropyOfThePassword;
+        if (numberOfPossibleCharacters === 0) {
+            entropyOfThePassword = 0;
+        } else {
+            entropyOfThePassword = Math.log2(numberOfPossibleCharacters)*passwordValue.length;
+        }
+        //console.log('entropyOfThePassword: ' + entropyOfThePassword); //sprawdzacz
+        return entropyOfThePassword;
+    }
+
+    function passwordContainsCharacters(passwordValue, lowerBound, upperBound) {
+        for (i = 0; i < passwordValue.length; i++) {
+            if (passwordValue.charAt(i) >= lowerBound && passwordValue.charAt(i) <= upperBound) {
+               // console.log('password contains character beetween ' + lowerBound + ' and ' + upperBound); //sprawdzacz
                 return true;
             }
         }
@@ -20,126 +58,47 @@
                  (passwordValue.charAt(i) >= ":" && passwordValue.charAt(i) <= "@") ||
                  (passwordValue.charAt(i) >= "[" && passwordValue.charAt(i) <= "`") ||
                  (passwordValue.charAt(i) >= "{" && passwordValue.charAt(i) <= "~") ) {
-                console.log("special");
+                //console.log('special');   //sprawdzacz
                 return true;
             }
         }
     }
 
-    function calculateEntropyOfThePassword(passwordValue) {
-        var numberOfPossibleCharacters = 0;
-        if (passwordContainsCharacters(passwordValue, "a", "z")) {
-            numberOfPossibleCharacters += 26;
-        }
-        if (passwordContainsCharacters(passwordValue, "A", "Z")) {
-            numberOfPossibleCharacters += 26;
-        }
-        if (passwordContainsCharacters(passwordValue, "0", "9")) {
-            numberOfPossibleCharacters += 10;
-        }
-        if (areSpecialCharactersInPassword(passwordValue)) {
-            numberOfPossibleCharacters += 33;
-        }
-        console.log("number of possible chars: " + numberOfPossibleCharacters);
-        var entropyOfPassword;
-        if (numberOfPossibleCharacters === 0) {
-            entropyOfPassword = 0;
-        } else {
-            entropyOfPassword = Math.log2(numberOfPossibleCharacters)*passwordValue.length;
-        }
-        return entropyOfPassword;
-    }
-
-    function calculateStrengthOfThePassword(passwordEntropy, entropyOfSolidPassword) {
+    function caluculateStrengthOfThePassword(passwordEntropy, entropyOfStrongPassword) {
         var passwordStrength;
-        if (entropyOfSolidPassword != 0) {
-            passwordStrength = passwordEntropy / entropyOfSolidPassword;
+        if (entropyOfStrongPassword != 0) {
+            passwordStrength = passwordEntropy / entropyOfStrongPassword;
         } else {
-            passwordStrength = 1;
+            if (passwordEntropy != 0) {
+                passwordStrength = 1;
+            } else {
+                passwordStrength = 0;
+            }
         }
+        //console.log('entropyOfStrongPassword: ' + entropyOfStrongPassword);   //sprawdzacz
+        //console.log('passwordStrength: ' + passwordStrength);   //sprawdzacz
         return passwordStrength;
     }
 
-    function displayTheBar(passwordStrengthDivId, passwordStrength) {
-        if (passwordStrength = 0) {
-            $(weakPasswordDiv).text("Weak");
-            $(mediumPasswordDiv).text("Medium");
-            $(strongPasswordDiv).text("Strong");
-            $(weakPasswordDiv).css("background-color", "white");
-            $(mediumPasswordDiv).css("background-color", "white");
-            $(strongPasswordDiv).css("background-color", "white");
-        } else if (passwordStrength > 0 && passwordStrength <= 1/3) {
-            $(weakPasswordDiv).text("Weak");
-            $(mediumPasswordDiv).text("Medium");
-            $(strongPasswordDiv).text("Strong");
-            $(weakPasswordDiv).css("background-color", "red");
-            $(mediumPasswordDiv).css("background-color", "white");
-            $(strongPasswordDiv).css("background-color", "white");
-        } else if (passwordStrength > 1/3 && passwordStrength <= 2/3) {
-            $(weakPasswordDiv).text("");
-            $(mediumPasswordDiv).text("Medium");
-            $(strongPasswordDiv).text("Strong");
-            $(weakPasswordDiv).css("background-color", "yellow");
-            $(mediumPasswordDiv).css("background-color", "yellow");
-            $(strongPasswordDiv).css("background-color", "white");
-        } else {
-            $(weakPasswordDiv).text("");
-            $(mediumPasswordDiv).text("");
-            $(strongPasswordDiv).text("Strong");
-            $(weakPasswordDiv).css("background-color", "green");
-            $(mediumPasswordDiv).css("background-color", "green");
-            $(strongPasswordDiv).css("background-color", "green");
-        }
-    }
-
-    function initializePasswordStrengthDisplay(passwordStrengthDivId) {
-        var weakPasswordDiv = document.createElement("div");
-        var mediumPasswordDiv = document.createElement("div");
-        var strongPasswordDiv = document.createElement("div");
-        $(weakPasswordDiv).attr("id", "weakPasswordDiv");
-        $(mediumPasswordDiv).attr("id", "mediumPasswordDiv");
-        $(strongPasswordDiv).attr("id", "strongPasswordDiv");
-        $(passwordStrengthDivId).append(weakPasswordDiv);
-        $(passwordStrengthDivId).append(mediumPasswordDiv);
-        $(passwordStrengthDivId).append(strongPasswordDiv);
-        $("#weakPasswordDiv").width(100);
-        $("#weakPasswordDiv").height(30);
-        $("#weakPasswordDiv").text("Weak");
-        $("#weakPasswordDiv").css("display", "inline-block");
-        $("#mediumPasswordDiv").width(100);
-        $("#mediumPasswordDiv").height(30);
-        $("#mediumPasswordDiv").text("Medium");
-        $("#mediumPasswordDiv").css("display", "inline-block");
-        $("#strongPasswordDiv").width(100);
-        $("#strongPasswordDiv").height(30);
-        $("#strongPasswordDiv").text("Strong");
-        $("#strongPasswordDiv").css("display", "inline-block");
-    }
-
-    function displayThePasswordStrengthBar(passwordEntropy, entropyOfSolidPassword) {
-        var passwordStrength = calculateStrengthOfThePassword(passwordEntropy, entropyOfSolidPassword);
-        displayTheBar(passwordStrength);
+    function displayTheBar(passwordStrength, passwordStrengthMeterId) {
+        document.getElementById(passwordStrengthMeterId).value = passwordStrength;
     }
 
     $.fn.addPasswordStrengthChecker = function(options) {   //to jest nasza funkcja główna w niej wszystko się dzieje
         //parametry naszego pluginu
         var parameters = $.extend({
-            passwordStrengthDivId : "#passwordStrengthBar",
-            entropyValueOfSolidPassword : "47.5"   //odpowiednik entropi dla małych, dużych liter oraz cyfr 8 znaków
+            passwordStrengthMeterId : "#password-strength-meter",
+            entropyValueOfStrongPassword : "47.5"   //odpowiednik entropi dla małych, dużych liter oraz cyfr 8 znaków
         }, options);
 
         //serce naszego pluginu
         return this.each(function() {   //dzięki return zapewniamy łańcuchowosć przez each przechodzimy po wszystkich
                                         //elementach, które został przekazane do pluginu i dla każdego się on wywołuje
-            initializePasswordStrengthDisplay(parameters.passwordStrengthDivId);
-            $(this).bind("keyup", function() {
-                var passwordFieldValue = $(this).val();   //podstawienie aktualnego obiektu pod zmienna p[asswordField
-                console.log(passwordFieldValue);
-                var entropyOfPassword = calculateEntropyOfThePassword(passwordFieldValue);
-                console.log("entropy of password: " + entropyOfPassword);
-                displayThePasswordStrengthBar(entropyOfPassword, parameters.entropyValueOfSolidPassword);
+            createPasswordStrengthMeter($(this), parameters.passwordStrengthMeterId);
+            $(this).bind('keyup', function() {
+                modifyPasswordStrengthMeter($(this).val(), parameters.entropyValueOfStrongPassword, parameters.passwordStrengthMeterId);
             });
-
         });
     }
+
 })(jQuery);
--- script.js
+++ script.js
@@ -2,7 +2,6 @@
  * Created by Janek on 2016-04-19.
  */
 $(document).ready(function() {
-    console.log("Hey");
-   $("#passwordField").addPasswordStrengthChecker({passwordStrengthDivId : "#passwordStrengthDiv"});
-   console.log("Hey");
+    $('#password').addPasswordStrengthChecker();
 });
+
